4b8946e86e86256983621e3b4670f2effed807be
wendelinspoerk <wendelinsp@edu.aau.at>
2022-04-19 21:34:38 +0200

    added some filse, drawable + Lobby activity

---
+++ app/src/main/java/at/aau/se2/chessify/LobbyActivity.java
@@ -0,0 +1,14 @@
+package at.aau.se2.chessify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LobbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lobby);
+    }
+}
