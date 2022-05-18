352804e3d18d8c97f78878bf13fc6bdcf57e8dd1
wendelinspoerk <wendelinsp@edu.aau.at>
2022-05-18 07:41:30 +0200

    ActivityTests added (Main, Settings, Lobby, Dice)

---
+++ app/src/androidTest/java/at/aau/se2/chessify/espressoTests/SettingActivityTest.java
@@ -0,0 +1,54 @@
+package at.aau.se2.chessify.espressoTests;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import at.aau.se2.chessify.R;
+import at.aau.se2.chessify.SettingActivity;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class SettingActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<SettingActivity> activityRule =
+            new ActivityScenarioRule<>(SettingActivity.class);
+
+    @Test
+    public void textFieldSettings() {
+        onView(withText("Settings")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void textFieldSettings2() {
+        onView(withText("SOUND ON")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void textFieldSettings3() {
+        onView(withText("VIBRATIONS ON")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void textFieldSettings4() {
+        onView(withText("DARKMODE OFF")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void CheckIntent() {
+        onView(ViewMatchers.withId(R.id.btn_sound)).perform(click());
+    }
+
+}
