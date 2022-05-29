dea7fe9b99badbc018fcda459b9c05970cf47915
Phalangs <p1miklautz@edu.aau.at>
2022-05-29 19:25:18 +0200

    Refactored BishopTest class for better readability.

--- app/src/test/java/at/aau/se2/chessify/chessLogicTests/pieces/BishopTest.java
+++ app/src/test/java/at/aau/se2/chessify/chessLogicTests/pieces/BishopTest.java
@@ -1,6 +1,7 @@
 package at.aau.se2.chessify.chessLogicTests.pieces;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import at.aau.se2.chessify.chessLogic.board.*;
 import at.aau.se2.chessify.chessLogic.pieces.*;
@@ -23,8 +24,32 @@ public class BishopTest {
 
     @Test
     public void getLegalMovesBishopTest(){
+        //setup board
+        testChessBoard=initializeTestChessboardBishop();
 
-        initializeTestChessboardBishop();
+        //get locations manually and with method
+        ArrayList<Location> possibleMovesArray = setLegalMovesBishop();
+        ArrayList<Location> legalMovesMethodArray = testBishop.getLegalMoves(testChessBoard);
+
+        //compare location lists
+        assertEquals(possibleMovesArray.size(), legalMovesMethodArray.size());
+        for(Location loc : possibleMovesArray){
+            assertTrue(loc.checkIfLocationIsPartOfList(legalMovesMethodArray));
+        }
+        for(Location loc : legalMovesMethodArray){
+            assertTrue(loc.checkIfLocationIsPartOfList(possibleMovesArray));
+        }
+    }
+
+    private ChessBoard initializeTestChessboardBishop(){
+        testChessBoard=new ChessBoard();
+        testChessBoard.setGameBoard(manuallyGeneratedTestBoard);
+        testBishop= new Bishop(PieceColour.BLACK);
+        testChessBoard.getGameBoard()[3][4]=testBishop;
+        return testChessBoard;
+    }
+
+    private ArrayList<Location> setLegalMovesBishop(){
         ArrayList<Location> possibleMovesArray = new ArrayList<>();
 
         possibleMovesArray.add(new Location(4,3));
@@ -44,30 +69,44 @@ public class BishopTest {
         possibleMovesArray.add(new Location(5,6));
         possibleMovesArray.add(new Location(6,7));
 
-        //assertEquals(possibleMovesArray, testBishop.getLegalMoves(testChessBoard));
-        ArrayList<Location> legalMovesMethodArray = testBishop.getLegalMoves(testChessBoard);
-        assertEquals(possibleMovesArray.size(), legalMovesMethodArray.size());
-
-        for(Location loc : possibleMovesArray){
-            assertEquals(true, loc.checkIfLocationIsPartOfList(legalMovesMethodArray));
-        }
-        for(Location loc : legalMovesMethodArray){
-            assertEquals(true, loc.checkIfLocationIsPartOfList(possibleMovesArray));
-        }
-    }
-
-    private void initializeTestChessboardBishop(){
-        testChessBoard=new ChessBoard();
-        testChessBoard.setGameBoard(manuallyGeneratedTestBoard);
-        testBishop= new Bishop(PieceColour.BLACK);
-        testChessBoard.getGameBoard()[3][4]=testBishop;
-
+        return possibleMovesArray;
     }
 
     @Test
     public void getLegalMovesBishopTestEnemy(){
+        //setup board
+        testChessBoard=initializeTestChessboardBishopEnemy();
 
-        initializeTestChessboardBishopEnemy();
+        //get locations manually and with method
+        ArrayList<Location> possibleMovesArray = setLegalMovesBishopEnemy();
+        ArrayList<Location> legalMovesMethodArray = testBishop.getLegalMoves(testChessBoard);
+
+        //compare location lists
+        assertEquals(possibleMovesArray.size(), legalMovesMethodArray.size());
+        for(Location loc : possibleMovesArray){
+            assertTrue(loc.checkIfLocationIsPartOfList(legalMovesMethodArray));
+        }
+        for(Location loc : legalMovesMethodArray){
+            assertTrue(loc.checkIfLocationIsPartOfList(possibleMovesArray));
+        }
+
+    }
+
+    private ChessBoard initializeTestChessboardBishopEnemy(){
+        testChessBoard=new ChessBoard();
+        testChessBoard.setGameBoard(manuallyGeneratedTestBoard);
+        testBishop= new Bishop(PieceColour.BLACK);
+        testChessBoard.getGameBoard()[3][4]=testBishop;
+        testChessBoard.getGameBoard()[1][2]=new Pawn(PieceColour.WHITE);
+        testChessBoard.getGameBoard()[0][7]=new Pawn(PieceColour.WHITE);
+
+        testChessBoard.getGameBoard()[4][5]=new Pawn(PieceColour.WHITE);
+        testChessBoard.getGameBoard()[5][2]=new Pawn(PieceColour.WHITE);
+
+        return testChessBoard;
+    }
+
+    private ArrayList<Location> setLegalMovesBishopEnemy(){
         ArrayList<Location> possibleMovesArray = new ArrayList<>();
 
         possibleMovesArray.add(new Location(4,3));
@@ -83,58 +122,30 @@ public class BishopTest {
 
         possibleMovesArray.add(new Location(4,5));
 
-
-        ArrayList<Location> legalMovesMethodArray = testBishop.getLegalMoves(testChessBoard);
-        assertEquals(possibleMovesArray.size(), legalMovesMethodArray.size());
-
-        for(Location loc : possibleMovesArray){
-            assertEquals(true, loc.checkIfLocationIsPartOfList(legalMovesMethodArray));
-        }
-        for(Location loc : legalMovesMethodArray){
-            assertEquals(true, loc.checkIfLocationIsPartOfList(possibleMovesArray));
-        }
-
-    }
-
-    private void initializeTestChessboardBishopEnemy(){
-        testChessBoard=new ChessBoard();
-        testChessBoard.setGameBoard(manuallyGeneratedTestBoard);
-        testBishop= new Bishop(PieceColour.BLACK);
-        testChessBoard.getGameBoard()[3][4]=testBishop;
-        testChessBoard.getGameBoard()[1][2]=new Pawn(PieceColour.WHITE);
-        testChessBoard.getGameBoard()[0][7]=new Pawn(PieceColour.WHITE);
-
-        testChessBoard.getGameBoard()[4][5]=new Pawn(PieceColour.WHITE);
-        testChessBoard.getGameBoard()[5][2]=new Pawn(PieceColour.WHITE);
+        return possibleMovesArray;
     }
 
     @Test
     public void getLegalMovesBishopTestAlly(){
+        //setup board
+        testChessBoard=initializeTestChessboardBishopAlly();
 
-        initializeTestChessboardBishopAlly();
-        ArrayList<Location> possibleMovesArray = new ArrayList<>();
-
-        possibleMovesArray.add(new Location(4,3));
-
-        possibleMovesArray.add(new Location(2,5));
-        possibleMovesArray.add(new Location(1,6));
-
-        possibleMovesArray.add(new Location(2,3));
-
-
+        //get locations manually and with method
+        ArrayList<Location> possibleMovesArray = setLegalMovesBishopAlly();
         ArrayList<Location> legalMovesMethodArray = testBishop.getLegalMoves(testChessBoard);
-        assertEquals(possibleMovesArray.size(), legalMovesMethodArray.size());
 
+        //compare location lists
+        assertEquals(possibleMovesArray.size(), legalMovesMethodArray.size());
         for(Location loc : possibleMovesArray){
-            assertEquals(true, loc.checkIfLocationIsPartOfList(legalMovesMethodArray));
+            assertTrue(loc.checkIfLocationIsPartOfList(legalMovesMethodArray));
         }
         for(Location loc : legalMovesMethodArray){
-            assertEquals(true, loc.checkIfLocationIsPartOfList(possibleMovesArray));
+            assertTrue(loc.checkIfLocationIsPartOfList(possibleMovesArray));
         }
 
     }
 
-    private void initializeTestChessboardBishopAlly(){
+    private ChessBoard initializeTestChessboardBishopAlly(){
         testChessBoard=new ChessBoard();
         testChessBoard.setGameBoard(manuallyGeneratedTestBoard);
         testBishop= new Bishop(PieceColour.BLACK);
@@ -144,6 +155,21 @@ public class BishopTest {
 
         testChessBoard.getGameBoard()[4][5]=new Pawn(PieceColour.BLACK);
         testChessBoard.getGameBoard()[5][2]=new Pawn(PieceColour.BLACK);
+
+        return testChessBoard;
+    }
+
+    private ArrayList<Location> setLegalMovesBishopAlly(){
+        ArrayList<Location> possibleMovesArray = new ArrayList<>();
+
+        possibleMovesArray.add(new Location(4,3));
+
+        possibleMovesArray.add(new Location(2,5));
+        possibleMovesArray.add(new Location(1,6));
+
+        possibleMovesArray.add(new Location(2,3));
+
+        return possibleMovesArray;
     }
 
 }
