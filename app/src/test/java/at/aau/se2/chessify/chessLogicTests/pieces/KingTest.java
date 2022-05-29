bab889edba5177586f45e45b5bc54e3a6887655d
Phalangs <p1miklautz@edu.aau.at>
2022-05-29 19:36:20 +0200

    Refactored KingTest class for better readability.

--- app/src/test/java/at/aau/se2/chessify/chessLogicTests/pieces/KingTest.java
+++ app/src/test/java/at/aau/se2/chessify/chessLogicTests/pieces/KingTest.java
@@ -1,6 +1,7 @@
 package at.aau.se2.chessify.chessLogicTests.pieces;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import at.aau.se2.chessify.chessLogic.board.*;
 import at.aau.se2.chessify.chessLogic.pieces.*;
@@ -23,44 +24,33 @@ public class KingTest {
 
     @Test
     public void getLegalMovesKingTest(){
+        //setup board
+        testChessBoard=initializeTestChessboardKing();
 
-        initializeTestChessboardKing();
-        ArrayList<Location> possibleMovesArray = new ArrayList<>();
-
-        possibleMovesArray.add(new Location(4,3));
-        possibleMovesArray.add(new Location(2,5));
-        possibleMovesArray.add(new Location(2,3));
-        possibleMovesArray.add(new Location(4,5));
-
-        possibleMovesArray.add(new Location(2,4));
-        possibleMovesArray.add(new Location(3,3));
-        possibleMovesArray.add(new Location(3,5));
-        possibleMovesArray.add(new Location(4,4));
-
-
+        //get locations manually and with method
+        ArrayList<Location> possibleMovesArray = setLegalMovesKing();
         ArrayList<Location> legalMovesMethodArray = testKing.getLegalMoves(testChessBoard);
-        assertEquals(possibleMovesArray.size(), legalMovesMethodArray.size());
 
+        //compare location lists
+        assertEquals(possibleMovesArray.size(), legalMovesMethodArray.size());
         for(Location loc : possibleMovesArray){
-            assertEquals(true, loc.checkIfLocationIsPartOfList(legalMovesMethodArray));
+            assertTrue(loc.checkIfLocationIsPartOfList(legalMovesMethodArray));
         }
         for(Location loc : legalMovesMethodArray){
-            assertEquals(true, loc.checkIfLocationIsPartOfList(possibleMovesArray));
+            assertTrue(loc.checkIfLocationIsPartOfList(possibleMovesArray));
         }
     }
 
-    private void initializeTestChessboardKing(){
-        testChessBoard=new ChessBoard();
+    private ChessBoard initializeTestChessboardKing(){
+        ChessBoard testChessBoard=new ChessBoard();
         testChessBoard.setGameBoard(manuallyGeneratedTestBoard);
         testKing= new King(PieceColour.BLACK);
         testChessBoard.getGameBoard()[3][4]=testKing;
 
+        return testChessBoard;
     }
 
-    @Test
-    public void getLegalMovesKingTestEnemy(){
-
-        initializeTestChessboardKingEnemy();
+    private ArrayList<Location> setLegalMovesKing(){
         ArrayList<Location> possibleMovesArray = new ArrayList<>();
 
         possibleMovesArray.add(new Location(4,3));
@@ -73,21 +63,31 @@ public class KingTest {
         possibleMovesArray.add(new Location(3,5));
         possibleMovesArray.add(new Location(4,4));
 
+        return possibleMovesArray;
+    }
 
+    @Test
+    public void getLegalMovesKingTestEnemy(){
+        //setup board
+        testChessBoard=initializeTestChessboardKingEnemy();
+
+        //get locations manually and with method
+        ArrayList<Location> possibleMovesArray = setLegalMovesKingEnemy();
         ArrayList<Location> legalMovesMethodArray = testKing.getLegalMoves(testChessBoard);
-        assertEquals(possibleMovesArray.size(), legalMovesMethodArray.size());
 
+        //compare location lists
+        assertEquals(possibleMovesArray.size(), legalMovesMethodArray.size());
         for(Location loc : possibleMovesArray){
-            assertEquals(true, loc.checkIfLocationIsPartOfList(legalMovesMethodArray));
+            assertTrue(loc.checkIfLocationIsPartOfList(legalMovesMethodArray));
         }
         for(Location loc : legalMovesMethodArray){
-            assertEquals(true, loc.checkIfLocationIsPartOfList(possibleMovesArray));
+            assertTrue(loc.checkIfLocationIsPartOfList(possibleMovesArray));
         }
 
     }
 
-    private void initializeTestChessboardKingEnemy(){
-        testChessBoard=new ChessBoard();
+    private ChessBoard initializeTestChessboardKingEnemy(){
+        ChessBoard testChessBoard=new ChessBoard();
         testChessBoard.setGameBoard(manuallyGeneratedTestBoard);
         testKing= new King(PieceColour.BLACK);
         testChessBoard.getGameBoard()[3][4]=testKing;
@@ -96,12 +96,61 @@ public class KingTest {
 
         testChessBoard.getGameBoard()[2][5]=new Pawn(PieceColour.WHITE);
         testChessBoard.getGameBoard()[5][2]=new Pawn(PieceColour.WHITE);
+
+        return testChessBoard;
+    }
+
+    private ArrayList<Location> setLegalMovesKingEnemy(){
+        ArrayList<Location> possibleMovesArray = new ArrayList<>();
+
+        possibleMovesArray.add(new Location(4,3));
+        possibleMovesArray.add(new Location(2,5));
+        possibleMovesArray.add(new Location(2,3));
+        possibleMovesArray.add(new Location(4,5));
+
+        possibleMovesArray.add(new Location(2,4));
+        possibleMovesArray.add(new Location(3,3));
+        possibleMovesArray.add(new Location(3,5));
+        possibleMovesArray.add(new Location(4,4));
+
+        return possibleMovesArray;
     }
 
     @Test
     public void getLegalMovesKingTestAlly(){
+        //setup board
+        testChessBoard=initializeTestChessboardKingAlly();
 
-        initializeTestChessboardKingAlly();
+        //get locations manually and with method
+        ArrayList<Location> possibleMovesArray = setLegalMovesKingAlly();
+        ArrayList<Location> legalMovesMethodArray = testKing.getLegalMoves(testChessBoard);
+
+        //compare location lists
+        assertEquals(possibleMovesArray.size(), legalMovesMethodArray.size());
+        for(Location loc : possibleMovesArray){
+            assertTrue(loc.checkIfLocationIsPartOfList(legalMovesMethodArray));
+        }
+        for(Location loc : legalMovesMethodArray){
+            assertTrue(loc.checkIfLocationIsPartOfList(possibleMovesArray));
+        }
+
+    }
+
+    private ChessBoard initializeTestChessboardKingAlly(){
+        ChessBoard testChessBoard=new ChessBoard();
+        testChessBoard.setGameBoard(manuallyGeneratedTestBoard);
+        testKing= new King(PieceColour.BLACK);
+        testChessBoard.getGameBoard()[3][4]=testKing;
+        testChessBoard.getGameBoard()[2][3]=new Pawn(PieceColour.BLACK);
+        testChessBoard.getGameBoard()[4][4]=new Pawn(PieceColour.BLACK);
+
+        testChessBoard.getGameBoard()[2][5]=new Pawn(PieceColour.BLACK);
+        testChessBoard.getGameBoard()[5][2]=new Pawn(PieceColour.BLACK);
+
+        return testChessBoard;
+    }
+
+    private ArrayList<Location> setLegalMovesKingAlly(){
         ArrayList<Location> possibleMovesArray = new ArrayList<>();
 
         possibleMovesArray.add(new Location(4,3));
@@ -113,29 +162,7 @@ public class KingTest {
         possibleMovesArray.add(new Location(3,3));
         possibleMovesArray.add(new Location(3,5));
 
-
-        ArrayList<Location> legalMovesMethodArray = testKing.getLegalMoves(testChessBoard);
-        assertEquals(possibleMovesArray.size(), legalMovesMethodArray.size());
-
-        for(Location loc : possibleMovesArray){
-            assertEquals(true, loc.checkIfLocationIsPartOfList(legalMovesMethodArray));
-        }
-        for(Location loc : legalMovesMethodArray){
-            assertEquals(true, loc.checkIfLocationIsPartOfList(possibleMovesArray));
-        }
-
-    }
-
-    private void initializeTestChessboardKingAlly(){
-        testChessBoard=new ChessBoard();
-        testChessBoard.setGameBoard(manuallyGeneratedTestBoard);
-        testKing= new King(PieceColour.BLACK);
-        testChessBoard.getGameBoard()[3][4]=testKing;
-        testChessBoard.getGameBoard()[2][3]=new Pawn(PieceColour.BLACK);
-        testChessBoard.getGameBoard()[4][4]=new Pawn(PieceColour.BLACK);
-
-        testChessBoard.getGameBoard()[2][5]=new Pawn(PieceColour.BLACK);
-        testChessBoard.getGameBoard()[5][2]=new Pawn(PieceColour.BLACK);
+        return possibleMovesArray;
     }
 
 }
