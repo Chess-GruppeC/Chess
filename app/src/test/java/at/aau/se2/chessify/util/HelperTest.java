4ba02f1ac97a90318a564663ea4eb0accaa44792
wendelinspoerk <wendelinsp@edu.aau.at>
2022-05-31 16:06:45 +0200

    Test Class created

---
+++ app/src/test/java/at/aau/se2/chessify/util/HelperTest.java
@@ -0,0 +1,96 @@
+package at.aau.se2.chessify.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelperTest {
+
+    @Test
+    public void getSharedPreferences() {
+    }
+
+    @Test
+    public void getSharedPreferencesWithContext() {
+    }
+
+    @Test
+    public void getBackgroundSound() {
+    }
+
+    @Test
+    public void setBackgroundSound() {
+    }
+
+    @Test
+    public void getSMB_BarSound() {
+    }
+
+    @Test
+    public void setSMB_BarSound() {
+    }
+
+    @Test
+    public void getGameSound() {
+    }
+
+    @Test
+    public void setGameSound() {
+    }
+
+    @Test
+    public void getVibration() {
+    }
+
+    @Test
+    public void setVibration() {
+    }
+
+    @Test
+    public void getDarkmode() {
+    }
+
+    @Test
+    public void setDarkmode() {
+    }
+
+    @Test
+    public void getPlayerName() {
+    }
+
+    @Test
+    public void setPlayerName() {
+    }
+
+    @Test
+    public void setGameId() {
+    }
+
+    @Test
+    public void getGameId() {
+    }
+
+    @Test
+    public void playSMB_BarSound() {
+    }
+
+    @Test
+    public void stopSMB_BarSound() {
+    }
+
+    @Test
+    public void playMusicBackground() {
+    }
+
+    @Test
+    public void stopMusicBackground() {
+    }
+
+    @Test
+    public void playGameSound() {
+    }
+
+    @Test
+    public void stopGameSound() {
+    }
+}
